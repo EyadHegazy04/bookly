8ed83b5ac28b6dd71a0079c7ca8ea0b72c149c75
Eyad hegazy <3moeyad@gmail.com>
2024-02-23 18:20:46 +0200

    Add Custom Scrollview

--- lib/features/home/presention/views/widgets/book_details_view_body.dart
+++ lib/features/home/presention/views/widgets/book_details_view_body.dart
@@ -14,56 +14,66 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: Column(
-        children: [
-          const CustomBookDetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .27),
-            child: const CustomBookImage(),
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          const Text(
-            "The Jungle Book ",
-            style: Styles.textStyle30,
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-           Opacity(
-             opacity: .7,
-             child: Text(
-              "Rudyard Kipling  ",
-              style: Styles.textStyle18.copyWith(fontStyle:FontStyle.italic,
+    return CustomScrollView(
+      slivers: [SliverFillRemaining(
+        child:  Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          child: Column(
+            children: [
+              const CustomBookDetailsAppBar(),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * .27),
+                child: const CustomBookImage(),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              const Text(
+                "The Jungle Book ",
+                style: Styles.textStyle30,
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              Opacity(
+                opacity: .7,
+                child: Text(
+                  "Rudyard Kipling  ",
+                  style: Styles.textStyle18.copyWith(fontStyle:FontStyle.italic,
 
-              fontWeight: FontWeight.w800),
-                       ),
-           ),
-          const SizedBox(
-            height: 12,
+                      fontWeight: FontWeight.w800),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              const BookRating(mainAxisAlignment: MainAxisAlignment.center,),
+              const SizedBox(
+                height: 25,
+              ),
+              const BooksAction(),
+              const  Expanded(
+                child:  SizedBox(
+                  height: 30,
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text("You Can also Like",
+                    style: Styles.textStyle14.copyWith(
+                        fontWeight: FontWeight.w600
+                    )),
+              ),
+              const SizedBox(height: 16,),
+              const  SimilarBoxListView(),
+              const SizedBox(height: 30,),
+            ],
           ),
-       const BookRating(mainAxisAlignment: MainAxisAlignment.center,),
-          const SizedBox(
-            height: 25,
-          ),
-         const BooksAction(),
-          const SizedBox(
-            height: 30,
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text("You Can also Like",
-              style: Styles.textStyle14.copyWith(
-                fontWeight: FontWeight.w600
-              )),
-          ),
-        const SizedBox(height: 16,),
-        const  SimilarBoxListView()
-        ],
-      ),
+        ),
+      )],
     );
+
+
+
   }
 }
