801d3f12d0436a1ba7ffa55c1ea579bf1cafd96b
Eyad hegazy <3moeyad@gmail.com>
2024-02-23 03:07:22 +0200

    add title to details

--- lib/features/home/presention/views/widgets/book_details_view_body.dart
+++ lib/features/home/presention/views/widgets/book_details_view_body.dart
@@ -1,5 +1,6 @@
+import 'package:bookly/core/utils/styles.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'custom_book_details_appbar.dart';
 import 'custom_book_image.dart';
 
@@ -9,15 +10,34 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return  Padding(
-      padding: const  EdgeInsets.symmetric(horizontal: 30.0),
-      child:  Column(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: Column(
         children: [
-         const  CustomBookDetailsAppBar(),
+          const CustomBookDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .17 ),
+            padding: EdgeInsets.symmetric(horizontal: width * .2),
             child: const CustomBookImage(),
           ),
+          const SizedBox(
+            height: 43,
+          ),
+          const Text(
+            "The Jungle Book ",
+            style: Styles.textStyle30,
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+           Opacity(
+             opacity: .7,
+             child: Text(
+              "Rudyard Kipling  ",
+              style: Styles.textStyle18.copyWith(fontStyle:FontStyle.italic,
+
+              fontWeight: FontWeight.w800),
+                       ),
+           ),
         ],
       ),
     );
