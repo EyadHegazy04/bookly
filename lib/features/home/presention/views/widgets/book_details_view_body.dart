1130eeaa8f1602424001521f609dc2b7215fb601
Eyad hegazy <3moeyad@gmail.com>
2024-02-23 17:53:28 +0200

    create Box Action

--- lib/features/home/presention/views/widgets/book_details_view_body.dart
+++ lib/features/home/presention/views/widgets/book_details_view_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'book_rating.dart';
+import 'books_action.dart';
 import 'custom_book_details_appbar.dart';
 import 'custom_book_image.dart';
 
@@ -43,6 +44,10 @@ class BookDetailsViewBody extends StatelessWidget {
             height: 18,
           ),
        const BookRating(mainAxisAlignment: MainAxisAlignment.center,),
+          const SizedBox(
+            height: 37,
+          ),
+         const BooksAction()
         ],
       ),
     );
